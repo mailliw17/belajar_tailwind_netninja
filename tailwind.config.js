63fef5be946b5dcc783d4531b96d001e6b5922ec
William <william@detik.com>
2021-10-13 10:11:55 +0700

    ignore commit for nodemodules

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+        colors : {
+          primary : '#ff0000',
+          secondary : {
+            100 : '#E2E2D5',
+            200 : '#888883',
+          }
+        }
+    },
+  },
+  plugins: [],
+}
